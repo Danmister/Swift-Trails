e3960a774cf9f4b6352c1296753317b358b245a3
Danmister <dcorrea012@gmail.com>
2016-09-24 21:40:31 -0400

    My First Commit

--- Swift Trial/ViewController.swift
+++ Swift Trial/ViewController.swift
@@ -3,17 +3,36 @@
 //  Swift Trial
 //
 //  Created by Daniel Correa on 9/20/16.
-//  Copyright © 2016 Random App Center. All rights reserved.
+//  Copyright © 2016 Random App Center. All rights reserved and more..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBOutlet weak var secondLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+       tapCount = tapCount + 1
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button ten times!"
+        }
+    }
+    
+    @IBAction func secondButtonTapped(_ sender: AnyObject) {
+        secondLabel.text = "Buttons Are Cool!"
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
